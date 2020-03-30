6d15dc21038d18c97854cf731542920a4740d019
szilard-molnar <mszilard86@gmail.com>
2020-03-30 13:14:16 -0400

    client folder

---
+++ client/build/precache-manifest.e920b9dc744b4b9f8c14f41513aa5013.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6399cc6a86df1a412b1d42b775df0517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fed8a343f2614354d08",
+    "url": "/static/css/main.05dcc253.chunk.css"
+  },
+  {
+    "revision": "0d5bb6c354a2ad2be5ae",
+    "url": "/static/js/2.ab9292ab.chunk.js"
+  },
+  {
+    "revision": "fcdc5e536d87d4615bb78a0d5fa80be3",
+    "url": "/static/js/2.ab9292ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fed8a343f2614354d08",
+    "url": "/static/js/main.f4b582dd.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
